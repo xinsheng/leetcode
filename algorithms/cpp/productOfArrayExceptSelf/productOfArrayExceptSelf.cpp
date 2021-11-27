464c2425291a606fbfdf2745423a7593aa4b9539
Sandro <xinshg@gmail.com>
2021-11-27 19:51:02 +0800

    update method2 of Product of Array Except Self

--- algorithms/cpp/productOfArrayExceptSelf/productOfArrayExceptSelf.cpp
+++ algorithms/cpp/productOfArrayExceptSelf/productOfArrayExceptSelf.cpp
@@ -58,4 +58,28 @@ public:
     }
 };
 
+/*
+ * Idea:
+ * Get the product of right first.
+ */
+
+ 
+class Solution_2 {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,1);
+        
+        for(int i = n-2; i>=0; i--){
+            ans[i] = ans[i+1] * nums[i+1];
+        }
+        
+        int productLeft = 1;
+        for(int i = 0; i< n; i++){
+            ans[i] = ans[i] * productLeft;
+            productLeft *= nums[i];
+        }
+        return ans;
+    }
+};
 	
